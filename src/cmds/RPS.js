f8bcbd3f0ee415b2ac95d6fd361bd9662c25cfdb
Pyan <Pyan4444@gmail.com>
2021-01-03 17:12:08 -0500

    Update RPS.js

--- src/cmds/RPS.js
+++ src/cmds/RPS.js
@@ -1,47 +1,60 @@
-const Embed = require('../util/Embed')
-const Random = require('../util/Random')
+const Embed = require("../util/Embed");
+const Random = require("../util/Random");
 
 function GetOutcome(GameHand, Hand) {
-    return GameHand == 1 && Hand == 3 || GameHand == 2 && Hand == 1 || GameHand == 3 && Hand == 2 ? "Loss" : 
-    GameHand == 3 && Hand == 1 || GameHand == 1 && Hand == 2 || GameHand == 2 && Hand == 3 ? "Win" : 
-    GameHand == Hand ? "Tie" : null;
+	return (GameHand == 1 && Hand == 3) ||
+		(GameHand == 2 && Hand == 1) ||
+		(GameHand == 3 && Hand == 2)
+		? "Loss"
+		: (GameHand == 3 && Hand == 1) ||
+		  (GameHand == 1 && Hand == 2) ||
+		  (GameHand == 2 && Hand == 3)
+		? "Win"
+		: GameHand == Hand
+		? "Tie"
+		: null;
 }
 
 function HandToNum(hand) {
-    return hand == "rock" ? 1 : hand == "paper" ? 2 : hand == "scissors" ? 3 : null;
+	return hand == "rock"
+		? 1
+		: hand == "paper"
+		? 2
+		: hand == "scissors"
+		? 3
+		: hand;
 }
 
 function NumToHand(num) {
-    return num == 1 ? "rock" : num == 2 ? "paper" : num == 3 ? "scissors" : null;
+	return num == 1 ? "rock" : num == 2 ? "paper" : num == 3 ? "scissors" : num;
 }
 
 function Game(msg, Hand) {
-    const GameHand = Random(1, 3)
-    Hand = HandToNum(Hand)
-    const Outcome = GetOutcome(GameHand, Hand)
-    const RPSEmbed = Embed('Rock Paper Scissors', '', [
-        {name: 'Your Move', value: '`' + NumToHand(Hand) + '`'},
-        {name: 'Bot Move', value: '`' + NumToHand(GameHand) + '`'},
-        {name: 'Outcome', value: '`' + Outcome + '`'}
-    ])
-    msg.channel.send(RPSEmbed)
+	const GameHand = Random(1, 3);
+	const Outcome = GetOutcome(GameHand, HandToNum(Hand));
+	const RPSEmbed = Embed("Rock Paper Scissors", "", [
+		{ name: "Your Move", value: "`" + Hand + "`" },
+		{ name: "Bot Move", value: "`" + NumToHand(GameHand) + "`" },
+		{ name: "Outcome", value: "`" + Outcome + "`" },
+	]);
+	msg.channel.send(RPSEmbed);
 }
 
-var cmd = []
+var cmd = [];
 
-cmd.cmd = function(msg, args) {
-    if (args[0]) {
-        const Hand = args[0].toLowerCase()
-        if (Hand === 'rock' || Hand === 'paper' || Hand === 'scissors') {
-            Game(msg, Hand)
-        } else {
-            msg.channel.send('Please choose either Rock Paper or Scissors')
-        }
-    } else {
-        msg.channel.send('Please choose either Rock Paper or Scissors')
-    }
-}
+cmd.cmd = function (msg, args) {
+	if (args[0]) {
+		const Hand = args[0].toLowerCase();
+		if (Hand === "rock" || Hand === "paper" || Hand === "scissors") {
+			Game(msg, Hand);
+		} else {
+			msg.channel.send("Please choose either Rock Paper or Scissors");
+		}
+	} else {
+		msg.channel.send("Please choose either Rock Paper or Scissors");
+	}
+};
 
-cmd.desc = "Lets you play Rock Paper Scissors"
+cmd.desc = "Lets you play Rock Paper Scissors";
 
 module.exports = cmd;
